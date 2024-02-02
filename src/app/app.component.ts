e18ee29d20769fc0d63a50fbb763d338a345b371
Yash Pathak <www.yashpathak102@gmail.com>
2024-02-02 16:09:36 +0530

    formvalidation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
+  import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'formvalidation';
-}
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  export class AppComponent {
+    registerForm!:FormGroup
+    title = 'formvalidation';
+    submitted = false;
+
+    constructor(private formbuilder: FormBuilder){ }
+    
+    ngOnInit() {
+
+      this.registerForm = this.formbuilder.group({
+        firstName:['',Validators.required],
+        lastName:['',Validators.required],
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required,Validators.minLength(8)]]
+      })
+    }
+
+    onSubmit(){
+      this.submitted = true
+      if (this.registerForm.invalid){
+      return
+      }
+      alert("Success");
+    }
+  }
